7f13063947d7186a87341d5b6cc0814833cd93cb
Abayomi Popoola <abayomipopoola@gmail.com>
2022-06-28 17:14:43 +0100

    First commit

---
+++ src/test/java/com/dot/qa/WikipediaTest.java
@@ -0,0 +1,69 @@
+package com.dot.qa;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * This class verifies elements on the wikipedia homepage.
+ */
+public class WikipediaTest {
+
+    private Logger log = LoggerFactory.getLogger(WikipediaTest.class);
+
+    private WebDriver driver;
+    private ElementFinder finder;
+
+    @BeforeClass
+    public void setup() {
+
+        /*
+            WebDriverManager.chromedriver().setup() should automatically use the right
+             driver for your Chrome version.  If it does not, you can choose a version manually
+            see https://sites.google.com/a/chromium.org/chromedriver/downloads
+            and update it as needed.
+
+            WebDriverManager.chromedriver().version("74.0.3729.6").setup();
+        */
+
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        finder = new ElementFinder(driver);
+
+        // adjust timeout as needed
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        driver.get("https://www.wikipedia.org/");
+    }
+
+    @Test
+    public void sloganPresent() {
+
+        String sloganClass = "localized-slogan";
+        WebElement slogan = finder.findElement(By.className(sloganClass));
+
+        Assert.assertNotNull(slogan, String.format("Unable to find slogan div by class: %s", sloganClass));
+
+        log.info("Slogan text is {}", slogan.getText());
+
+        Assert.assertEquals(slogan.getText(), "The Free Encyclopedia");
+    }
+
+    @AfterClass
+    public void closeBrowser() {
+
+        if(driver!=null) {
+            driver.close();
+        }
+    }
+}
